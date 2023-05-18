73dad8f37883829761d2b9db630eda7d89f2d8f3
Anthony <anthony.almeida.villarreal@udla.edu.ec>
2023-05-18 12:17:28 -0500

    Ejercicio matriz diagonal terminado

--- Ejerciciomatrizdiagonal.c
+++ Ejerciciomatrizdiagonal.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 //Matriz donde el usuario ingrese dimensión e inicialiice en 0 y la diagonal principal contenga unos 
 int main(int argc, char const *argv[])
 {
@@ -18,6 +19,19 @@ int main(int argc, char const *argv[])
             matriz[i][j] = 0;
         }
     }
+    // Establecer 1 en la diagonal principal (si la matriz es cuadrada)
+    int dimension = (filas < columnas) ? filas : columnas;
+    for (int i = 0; i < dimension; i++) {
+        matriz[i][i] = 1;
+    }
     
+    // Imprimir la matriz resultante
+    printf("Matriz resultante:\n");
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
